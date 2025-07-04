5132d1db1c5ed8ef92ec6ad726a233c75a4f5cb7
jtg1811 <jeetgandhi1804@gmail.com>
2025-07-04 20:02:44 +0530

    Support new line as delimiter

--- string_calculator.py
+++ string_calculator.py
@@ -1,6 +1,5 @@
 def add(numbers) :
     if numbers == "" :
         return 0
-    if "," in numbers :
-        return sum([int(x) for x in numbers.split(",")])
-    return int(numbers)
+    numbers = numbers.replace("\n", ",")
+    return sum([int(x) for x in numbers.split(",")])
